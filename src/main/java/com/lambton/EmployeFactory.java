f5dd140027ab2eac58b75fda249c88dc81fdff53
macstudent <shijieugin3@gmail.com>
2019-10-04 19:47:02 -0400

    employee

--- src/main/java/com/lambton/EmployeFactory.java
+++ src/main/java/com/lambton/EmployeFactory.java
@@ -3,6 +3,10 @@ package com.lambton;
 public class EmployeFactory
 {
     //FActory class here and the data
+    public static Employee employeeType(int type,String nName,int mAge)
+    {
+        
+    }
 }
 
 
