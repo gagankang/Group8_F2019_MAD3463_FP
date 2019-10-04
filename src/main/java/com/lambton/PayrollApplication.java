eb4de4c48cf6d9eeefad35fca8e90faed366d389
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-10-04 16:41:54 -0700

    payroll object creation

--- src/main/java/com/lambton/PayrollApplication.java
+++ src/main/java/com/lambton/PayrollApplication.java
@@ -3,6 +3,7 @@ package com.lambton;
 public class PayrollApplication {
     public static void main(String[] args) {
         Employee e1 = EmployeFactory.employeeType(Constants.TYPE_COMISSION_EMPLOYEE, "Parikshit", 36);
-        
+        ((ComissionBasedPartTime) e1).setVehicle(1, "Bullet", "RJ-14-CD-2001");
+
     }
 }
