41569f8ef2fa057e1befbdfb26b972d85ed1bdac
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-04 18:21:35 -0400

    Intern employee

--- src/main/java/com/lambton/Constants.java
+++ src/main/java/com/lambton/Constants.java
@@ -4,7 +4,6 @@ public class Constants
 {
     public static final int TYPE_COMISSION_EMPLOYEE=1;
     public static final int TYPE_FIXED_EMPLOYEE=2;
-    
-
-
+    public static final int TYPE_FULLTIME_EMPLOYEE=3;
+    public static final int TYPE_INTERN_EMPLOYEE=4;
 }
