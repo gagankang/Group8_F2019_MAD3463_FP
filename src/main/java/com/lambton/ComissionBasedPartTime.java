cd8990aaec769aec3723d21718bc16ca047983cc
macstudent <shijieugin3@gmail.com>
2019-10-04 18:49:01 -0400

    constructor

--- src/main/java/com/lambton/ComissionBasedPartTime.java
+++ src/main/java/com/lambton/ComissionBasedPartTime.java
@@ -3,3 +3,7 @@ package com.lambton;
 public class ComissionBasedPartTime extends PartTime {
 
     private int mComissionPerc;
+    //Constructor
+    public ComissionBasedPartTime(String mName, int mAge) {
+        super(mName, mAge);
+    }
