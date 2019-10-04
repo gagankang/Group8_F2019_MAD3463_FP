ba0a39cd8e401eef4830509e085081d575c7a7d3
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-04 19:09:16 -0400

    Intern Constructor

--- src/main/java/com/lambton/Intern.java
+++ src/main/java/com/lambton/Intern.java
@@ -2,6 +2,10 @@ package com.lambton;
 
 public class Intern extends Employee {
     private String mSchool;
+    //COnstructor
+    public Intern(String mName, int mAge) {
+        super(mName,mAge);
+    }
 
    
 
