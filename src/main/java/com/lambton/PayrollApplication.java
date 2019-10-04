a3770b5cb28ba70b2c8944d8759bbc29d4959a91
RajdeepK-03 <chahal.rajdeep03@gmail.com>
2019-10-04 16:40:38 -0700

    payroll

--- src/main/java/com/lambton/PayrollApplication.java
+++ src/main/java/com/lambton/PayrollApplication.java
@@ -1,6 +1,8 @@
 package com.lambton;
 
-    public class PayrollApplication {
-
-
+public class PayrollApplication {
+    public static void main(String[] args) {
+        Employee e1 = EmployeFactory.employeeType(Constants.TYPE_COMISSION_EMPLOYEE, "Parikshit", 36);
+        
     }
+}
