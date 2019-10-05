c190d7881a55fa359ada0e72d565211a5c153833
Gaganpreet kaur <preetkang4511@gmail.com>
2019-10-04 20:18:51 -0400

    Project Completed

--- src/main/java/com/lambton/PayrollApplication.java
+++ src/main/java/com/lambton/PayrollApplication.java
@@ -1,7 +1,7 @@
 package com.lambton;
 public class PayrollApplication {
     public static void main(String[] args) {
-        //Create the object and pass data
+        
         Employee e1 = EmployeFactory.employeeType(Constants.TYPE_COMISSION_EMPLOYEE, "Shiji", 22);
         ((ComissionBasedPartTime) e1).setVehicle(1, "Bullet", "RJ-14-CD-2001");
         ((ComissionBasedPartTime) e1).setRates(10, 20, 10);
